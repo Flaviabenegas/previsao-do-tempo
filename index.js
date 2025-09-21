cdbac58c440dd87fc7ae8fbc05cf34935b8ad9ac
Flavia Benegas <flaviabenegas@gmail.com>
2025-09-21 10:46:09 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const express = require('express')
+const axios = require('axios')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+
+app.get('/previsao/:cidade', async (req, res) => {
+    try {
+        const cidade = req.params.cidade;
+        const API_key = process.env.API_key
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${API_key}&units=metric&lang=pt_br`
+        const respostaDaApi = await axios.get(apiUrl);
+
+        res.json(respostaDaApi.data)
+
+    } catch (error) {
+
+        console.error("Erro ao buscar dados do clima:", error.message)
+        res.status(500).json({ mensagem: 'Erro ao consultar o clima. Verifique o nome da cidade ou a chave da API.' })
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
+});
